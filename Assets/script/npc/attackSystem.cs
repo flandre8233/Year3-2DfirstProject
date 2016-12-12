77f89d95fab6f9e367a9b03372e53dfbbe376ce4
flandre8233 <flandre8233@gmail.com>
2016-12-12 13:17:33 +0800

    Revert "alpha 0.14"
    This reverts commit c8d6f09ac4a108e2d01cfe4b11d16f71da0cfb79.

--- Assets/script/npc/attackSystem.cs
+++ Assets/script/npc/attackSystem.cs
@@ -19,8 +19,8 @@ public class attackSystem : MonoBehaviour {
     bool isCastCombo = false;
     int ComboCounter = 0;
 
-    // Use this for initialization
-    void Start() {
+	// Use this for initialization
+	void Start () {
         rigid2d = GetComponentInParent<Rigidbody2D>();
         npcclass = GetComponentInParent<npcClass>();
         if (GameObject.FindGameObjectsWithTag("megumin_player").Length != 0) {
@@ -32,58 +32,78 @@ public class attackSystem : MonoBehaviour {
     }
 
     private void State_Complete(Spine.TrackEntry trackEntry) { //動作結果那時
-                                                               //throw new System.NotImplementedException();
+        //throw new System.NotImplementedException();
+        if (npcclass.TypeP == npcClass.Type.contorl) {
 
-        if (trackEntry.animation.name == "up_attack_sword" && trackEntry.trackIndex == 1) {
-            if (ComboCounter >= 2) {
-                attackGO.SetActive(false);
-                npcSkeletonAnimation.state.SetAnimation(1, "side_attack_sword", false);
-                attackGO.SetActive(true);
+
+            if (trackEntry.animation.name == "up_attack_sword" && trackEntry.trackIndex == 1 ) {
+                if (ComboCounter >= 2) {
+                    npcSkeletonAnimation.state.SetAnimation(1, "side_attack_sword", false);
+                }
+                else {
+                    attackCDLock = true;
+                    StartCoroutine("attackColdDown");
+                    rigid2d.velocity = new Vector2(0, rigid2d.velocity.y); //還原
+                    npcclass.CastAniP = npcClass.CastAni.onMovement;
+                }
+
             }
-            else {
-                attackGO.SetActive(false);
+
+            if (trackEntry.animation.name == "side_attack_sword" && trackEntry.trackIndex == 1) {
                 attackCDLock = true;
                 StartCoroutine("attackColdDown");
-                rigid2d.velocity = new Vector2(0, rigid2d.velocity.y); //還原
+                rigid2d.velocity = new Vector2(0, rigid2d.velocity.y);
                 npcclass.CastAniP = npcClass.CastAni.onMovement;
             }
 
+
+            //Debug.Log(trackIndex + " " + state.GetCurrent(trackIndex) + ": end");
         }
-
-        if (trackEntry.animation.name == "side_attack_sword" && trackEntry.trackIndex == 1) {
-            attackGO.SetActive(false);
-            attackCDLock = true; //進入CD
-            StartCoroutine("attackColdDown");
-            rigid2d.velocity = new Vector2(0, rigid2d.velocity.y);
-            npcclass.CastAniP = npcClass.CastAni.onMovement;
-        }
-
-
-        //Debug.Log(trackIndex + " " + state.GetCurrent(trackIndex) + ": end");
     }
 
     // Update is called once per frame
-    void Update() {
+    void Update () {
 
         if (npcclass.TypeP == npcClass.Type.contorl) { //player attack
             Debug.Log(ComboCounter);
             if (Input.GetMouseButtonUp(1) && !attackCDLock && !selectEnemySystem.openTargetLockDown) {  //玩家按下攻擊
-                attackFunction();
-
+                StartCoroutine("attackFunction");
+                
             }
 
         }
         else {
-            if (!attackCDLock) {
-                attackFunction();
+            if ( !attackCDLock ) {
+                StartCoroutine("attackFunction");
             }
         }
 
+        
+	
+	}
+
+    IEnumerator attackFunction() {
+        /*
+        if (!attackCDLock && npcclass.CastAniP != npcClass.CastAni.onAttack) {
+            attackCDLock = true;
+            if (attackGO != null) {
+                //StartCoroutine("spawnAttackSensorFunction");
+                Debug.Log("diudiudiud");
+                StartCoroutine("spawnAttackAni");
 
 
-    }
+            }
+            else {
+                gunspawn.Shot();
+            }
+            
+        }
 
-    void attackFunction() {
+        
+        yield return new WaitForSeconds(CD);
+        attackCDLock = false;
+    
+    */
         if (ComboCounter >= 2) {
             attackCDLock = true; //鎖上不讓再打
         }
@@ -91,18 +111,24 @@ public class attackSystem : MonoBehaviour {
             isCastCombo = true;
         }
 
-        if (!attackCDLock) {
+        if (!attackCDLock  ) {
 
             if (attackGO != null) {
                 //StartCoroutine("spawnAttackSensorFunction");
                 Debug.Log("diudiudiud");
                 ComboCounter++;
-                spawnAttackAni();
+                StartCoroutine("spawnAttackAni");
+
+
             }
             else {
                 gunspawn.Shot();
             }
+
         }
+
+
+        yield return 0;
     }
 
     IEnumerator attackColdDown() {
@@ -112,22 +138,36 @@ public class attackSystem : MonoBehaviour {
     }
 
 
-    void spawnAttackAni() {
+        IEnumerator spawnAttackAni() {
         npcclass.CastAniP = npcClass.CastAni.onAttack;
         //npcSkeletonAnimation.AnimationName = "side_attack_sword";
         if (ComboCounter == 1) {
             npcSkeletonAnimation.state.SetAnimation(1, "up_attack_sword", false);
-            attackGO.SetActive(true);
         }
 
         Spine.TrackEntry trackEntry1 = npcSkeletonAnimation.state.GetCurrent(1); //*******
         //velocityPart
 
-        rigid2d.velocity = new Vector2(0, rigid2d.velocity.y);
+        rigid2d.velocity = new Vector2( 0 , rigid2d.velocity.y);
         rigid2d.AddForce(transform.right * 300 * -rigid2d.transform.localScale.x, ForceMode2D.Force);
         //rigid2d.velocity -= new Vector2(3.5f * rigid2d.transform.localScale.x, 0);
         //rigid2d.transform.position += transform.right * Time.deltaTime * 100;
         Debug.Log("diudiudiud");
+        yield return 0;
     }
 
-}
+
+
+
+    IEnumerator spawnAttackSensorFunction() {
+        if (!attacksensor) {
+            attacksensor = true;
+            attackGO.SetActive(attacksensor);
+        }
+
+        yield return new WaitForSeconds(attackSensorCD);
+        attacksensor = false;
+        attackGO.SetActive(attacksensor);
+    }
+
+    }
