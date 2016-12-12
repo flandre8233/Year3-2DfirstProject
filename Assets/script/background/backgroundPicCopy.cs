062a1497cdabfddbea8fefbd9d4ed26b81c76b5e
flandre8233 <flandre8233@gmail.com>
2016-12-12 13:17:56 +0800

    Revert "Revert "alpha 0.14""
    This reverts commit 77f89d95fab6f9e367a9b03372e53dfbbe376ce4.

---
+++ Assets/script/background/backgroundPicCopy.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class backgroundPicCopy : MonoBehaviour {
+    public float scrollSpeed;
+    private Vector2 savedOffset;
+    private Renderer renderer;
+
+    void Start() {
+        renderer = GetComponent<Renderer>();
+        savedOffset = renderer.sharedMaterial.GetTextureOffset("_MainTex");
+    }
+
+    void Update() {
+        float x = Mathf.Repeat( (Time.time * scrollSpeed), 1f);
+        Vector2 offset = new Vector2(x-0.5f, savedOffset.y);
+        renderer.sharedMaterial.SetTextureOffset("_MainTex", offset);
+    }
+
+    void OnDisable() {
+        renderer.sharedMaterial.SetTextureOffset("_MainTex", savedOffset);
+    }
+}
