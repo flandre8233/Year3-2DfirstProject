e5223c506539abb7abf69719c78022baa543afbd
flandre8233 <flandre8233@gmail.com>
2016-12-12 16:55:06 +0800

    alpha 1.15

--- Assets/script/background/backgroundPicCopy.cs
+++ Assets/script/background/backgroundPicCopy.cs
@@ -2,18 +2,25 @@
 using System.Collections;
 
 public class backgroundPicCopy : MonoBehaviour {
+    public Camera camera;
     public float scrollSpeed;
     private Vector2 savedOffset;
     private Renderer renderer;
+    private Vector3 startPosition;
 
+    public float distance;
     void Start() {
         renderer = GetComponent<Renderer>();
         savedOffset = renderer.sharedMaterial.GetTextureOffset("_MainTex");
+        //startPosition = Vector3.Distance(camera.transform.position, Vector3.zero); 
     }
 
     void Update() {
-        float x = Mathf.Repeat( (Time.time * scrollSpeed), 1f);
-        Vector2 offset = new Vector2(x-0.5f, savedOffset.y);
+         distance = Vector3.Distance(new Vector3(camera.transform.position.x , 0,0), new Vector3(-100,0,0));
+        //float x = Mathf.Repeat( (distance * (scrollSpeed/1000.0f) ), 1f);
+        // Vector2 offset = new Vector2(x, savedOffset.y);
+
+        Vector2 offset = new Vector2( -(distance * (scrollSpeed / 1000.0f) ) , savedOffset.y);
         renderer.sharedMaterial.SetTextureOffset("_MainTex", offset);
     }
 
