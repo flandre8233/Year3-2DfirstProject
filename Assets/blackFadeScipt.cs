b7eb0c1da58bf7f63316edffca86be64fda2b2ce
gibson8233 <gibson8233@hotmail.com>
2017-03-07 11:04:16 +0800

    beta 1.086a

---
+++ Assets/blackFadeScipt.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class blackFadeScipt : MonoBehaviour {
+    [SerializeField]
+    bool isStartAni;
+    [SerializeField]
+    bool isNeedFadeIn = false;
+    [SerializeField]
+    Animator ani;
+    [SerializeField]
+    RectTransform Rts;
+
+    bool DoOnce;
+
+	// Use this for initialization
+	void Start () {
+        ani.Stop(); //here
+        Rts.anchorMax = new Vector2(1,1);
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isStartAni&& !DoOnce) {
+            DoOnce = true;
+            if (isNeedFadeIn) {
+                ani.Play("BlackFadeInnimation");
+            }
+            else {
+                ani.Play("blackFadeOut");
+            }
+        }
+	}
+}
